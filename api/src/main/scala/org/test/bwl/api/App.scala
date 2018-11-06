1e4b5b0954ddaf60f9a45426270b7fb605645737
yykhomenko <yykhomenko@gmail.com>
2018-11-06 13:33:02 +0200

    - rename project to bwl

--- api/src/main/scala/org/test/bwl/api/App.scala
+++ api/src/main/scala/org/test/bwl/api/App.scala
@@ -5,7 +5,6 @@ import akka.http.scaladsl.server.HttpApp
 object App extends HttpApp {
 
   def main(args: Array[String]): Unit = {
-    // todo rename project to bwl
     startServer("0.0.0.0", 8080)
   }
 
