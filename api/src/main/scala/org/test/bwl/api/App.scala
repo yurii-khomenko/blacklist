02bbf2d447c602b91f11fd1b86017a338edff704
yykhomenko <yykhomenko@gmail.com>
2018-12-14 14:02:56 +0200

    - added pass api

--- api/src/main/scala/org/test/bwl/api/App.scala
+++ api/src/main/scala/org/test/bwl/api/App.scala
@@ -1,5 +1,7 @@
 package org.test.bwl.api
 
+import akka.http.scaladsl.model.HttpEntity
+import akka.http.scaladsl.model.MediaTypes.`application/json`
 import akka.http.scaladsl.server.HttpApp
 import org.test.bwl.model.conf.DB
 
@@ -8,22 +10,31 @@ object App extends HttpApp with DB {
   def main(args: Array[String]): Unit =
     startServer("0.0.0.0", 8080)
 
-  override def routes = path("api" / "rules" / LongNumber) { msisdn =>
+  override def routes = path("api" / "pass") {
 
-    val rule = blackListRuleAccessor.get(msisdn).one
+    get {
 
-    complete(s"Hello, msisdn: $msisdn, rule: $rule")
+      parameters('msisdn.as[Long], 'sn) { (msisdn, sn) =>
+
+        val rule = blackListRuleAccessor.get(msisdn).one
+
+        val pass = !rule.shortNumbers.contains(sn)
+
+        complete(HttpEntity(`application/json`, s"""{"passed":$pass}"""))
+      }
+    }
   }
 
   // GET  http://127.0.0.1:8080/api/rules/${msisdn}
 
   // GET  http://127.0.0.1:8080/api/rules/${msisdn}/${sn}
 
-  /*
 
+  // GET  http://127.0.0.1:8080/api/pass?msisdn=${msisdn}&sn=${sn}
+
+  /*
     {
       result: false
     }
-
-   */
+  */
 }
