c3b2cde58aff5127caa75b94b2add9306a001200
yykhomenko <yykhomenko@gmail.com>
2019-01-04 13:31:14 +0200

    - added api tests

--- api/src/test/scala/org/test/bwl/api/Config.scala
+++ api/src/test/scala/org/test/bwl/api/Config.scala
@@ -1,11 +1,23 @@
 package org.test.bwl.api
 
-import akka.http.scaladsl.model.headers.BasicHttpCredentials
+import akka.http.scaladsl.model.headers.{BasicHttpCredentials, HttpChallenge}
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import org.scalatest.{Matchers, WordSpec}
 import org.test.bwl.api.route.ApiRoute
 
 trait Config extends WordSpec with Matchers with ScalatestRouteTest with ApiRoute {
 
+  val responseWWWAuthHeader = HttpChallenge("Basic", Some("bwl"), Map("charset" → "UTF-8"))
+  val methodNotAllowed = "HTTP method not allowed, supported methods: GET, HEAD"
+  val requiresAuth = "The resource requires authentication, which was not supplied with the request"
+  val invalidAuth = "The supplied authentication is invalid"
+  val invalidRole = "You're don't have necessary role: "
+
+  val emptyCredentials = BasicHttpCredentials("", "")
+  val invalidCredentials = BasicHttpCredentials("Peter", "pan")
   val validClientCredentials = BasicHttpCredentials("test-client", "test-client-password")
+  val validMetricCredentials = BasicHttpCredentials("prometeus", "prometeus-password")
+
+  val msisdn = 380670000001L
+  val sn = 777
 }
--- api/src/test/scala/org/test/bwl/api/route/ApiRouteTest.scala
+++ api/src/test/scala/org/test/bwl/api/route/ApiRouteTest.scala
@@ -1,6 +1,8 @@
 package org.test.bwl.api.route
 
 import akka.http.scaladsl.model.StatusCodes._
+import akka.http.scaladsl.model.headers.`WWW-Authenticate`
+import akka.http.scaladsl.server.Route
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.test.bwl.api.Config
@@ -12,9 +14,55 @@ class ApiRouteTest extends Config {
 
   s"The hash system with REST resource $uri" should {
 
-    s"7. Return OK code with right body for GET requests to $uri with valid credentials" in {
+    s"1. Return Unauthorized error for GET requests to $uri without credentials" in {
 
-      Get(s"$uri?msisdn=380670000001&sn=777") ~>
+      Get(uri) ~>
+        Route.seal(routes) ~> check {
+
+        status shouldEqual Unauthorized
+        responseAs[String] shouldEqual requiresAuth
+        header[`WWW-Authenticate`].get.challenges.head shouldEqual responseWWWAuthHeader
+      }
+    }
+
+    s"2. Return Unauthorized error for GET requests to $uri with empty credentials" in {
+
+      Get(uri) ~>
+        addCredentials(invalidCredentials) ~>
+        Route.seal(routes) ~> check {
+
+        status shouldEqual Unauthorized
+        responseAs[String] shouldEqual invalidAuth
+        header[`WWW-Authenticate`].get.challenges.head shouldEqual responseWWWAuthHeader
+      }
+    }
+
+    s"3. Return Unauthorized error for GET requests to $uri with invalid credentials" in {
+
+      Get(uri) ~>
+        addCredentials(invalidCredentials) ~>
+        Route.seal(routes) ~> check {
+
+        status shouldEqual Unauthorized
+        responseAs[String] shouldEqual invalidAuth
+        header[`WWW-Authenticate`].get.challenges.head shouldEqual responseWWWAuthHeader
+      }
+    }
+
+//    s"4. Return Forbidden error with right error body for GET requests to $uri with other role credentials" in {
+//
+//      Get(uri) ~>
+//        addCredentials(validMetricCredentials) ~>
+//        Route.seal(routes) ~> check {
+//
+//        status shouldEqual Forbidden
+//        responseAs[String] shouldEqual invalidRole + ClientRole.role
+//      }
+//    }
+
+    s"5. Return OK code with right body for GET requests to $uri with valid credentials" in {
+
+      Get(s"$uri?msisdn=$msisdn&sn=$sn") ~>
         addCredentials(validClientCredentials) ~>
         routes ~> check {
 
